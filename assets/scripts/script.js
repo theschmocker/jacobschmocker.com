f56facac93c25197dd8b8f065287c78d7e30e0ed
Jacob Schmocker <jacobcs1996@gmail.com>
2017-10-15 22:49:52 -0500

    small refactor for readability. Corrected navbar behavior after window has been resized

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,52 +1,74 @@
-// ------------ Nav-bar color change on scroll -------------
-
-var landingHeight = document.getElementsByClassName('landing')[0].clientHeight,
+let landingHeight = document.getElementsByClassName('landing')[0].clientHeight,
     nav = document.getElementsByTagName('nav')[0],
     navHeight = nav.clientHeight,
     offset = landingHeight - navHeight;
 
-var scrolled = false;
+main();
 
-window.addEventListener('scroll', scroll);
-
-function scroll () {
-    scrolled = true;
+function main() {
+    setupMenuHandler();
+    setupScrollHandler();
+    setupResizeHandler();
 }
 
-function changeNav() {
-    if (window.pageYOffset >= offset) {
-        nav.classList.add('scroll');
-    } else {
-        nav.classList.remove('scroll');
+function setupScrollHandler() {
+    let scrolled = false;
+    
+    window.addEventListener('scroll', scroll);
+    
+    function scroll () {
+        scrolled = true;
+    }
+    
+    function changeNav() {
+        if (window.pageYOffset >= offset) {
+            nav.classList.add('scroll');
+        } else {
+            nav.classList.remove('scroll');
+        }
+    }
+    
+    setInterval(function() {
+        if( scrolled ) {
+            scrolled = false;
+            changeNav();
+        }
+    }, 100);
+    
+}
+
+function setupMenuHandler() {
+    // Detect any clicks on page, call toggleMenu
+    document.documentElement.addEventListener('click', toggleMenu);
+    
+    function toggleMenu(e) {
+        // if the menu button is pressed, open/close the menu
+        if (e.target.id === 'menu-button') {
+            menu.classList.toggle('open');
+            menu.classList.toggle('pressed');
+            nav.classList.toggle('menu-open');
+        } else {
+            // if anything other than the menu button is pressed, close the menu
+            menu.classList.remove('open');
+            menu.classList.remove('pressed');
+            nav.classList.remove('menu-open');
+        }
     }
 }
 
-setInterval(function() {
-    if( scrolled ) {
-        scrolled = false;
-        changeNav();
-    }
-}, 100);
+function setupResizeHandler() {
+    window.addEventListener("resize", resizeThrottler, false);
 
-// ----- Show menu on mobile -----
+    let resizeTimeout;
+    // 
+    function resizeThrottler() {
+        if (!resizeTimeout) {
+            resizeTimeout = setTimeout(() => {
+                resizeTimeout = null;
 
-let menuButton = document.getElementById('menu-button');
-let menu = document.getElementById('menu')
-
-// Detect any clicks on page, call toggleMenu
-document.documentElement.addEventListener('click', toggleMenu);
-
-function toggleMenu(e) {
-    // if the menu button is pressed, open/close the menu
-    if (e.target.id === 'menu-button') {
-        menu.classList.toggle('open');
-        menu.classList.toggle('pressed');
-        nav.classList.toggle('menu-open');
-    } else {
-        // if anything other than the menu button is pressed, close the menu
-        menu.classList.remove('open');
-        menu.classList.remove('pressed');
-        nav.classList.remove('menu-open');
+                landingHeight = document.getElementsByClassName('landing')[0].clientHeight;
+                offset = landingHeight - navHeight;
+            }, 500);
+        }
     }
 }
-
